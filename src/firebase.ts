41000fad84de393301b166056f3cab75cd1a4e82
vamsikrishna325 <vamsikrishna428117@gmail.com>
2026-01-24 20:12:57 +0530

    firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -15,7 +15,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
